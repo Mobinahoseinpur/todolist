686ce04885457cbaaa535eeea8060fe54493aed3
mobinaHoseinpur <74188525+Mobinahoseinpur@users.noreply.github.com>
2023-10-13 20:40:36 +0330

    create add note

--- assets/js/optionBtn.js
+++ assets/js/optionBtn.js
@@ -2,28 +2,59 @@
 const options = document.querySelectorAll('.options')
 const optionIcon = document.querySelectorAll('.optionIcon')
 const secBtn = document.querySelector('.secBtn')
+const counter =document.querySelector('.counter')
+const addBtn =document.querySelector('.addBtn')
+
 
 // open option menu
 let ـselected = 0
-console.log('test')
-options.forEach((value , index)=>{
+let _countT = ''
 
-    options[index].addEventListener('click' , function(){
-        ـselected=0
-        //peyda kardan bakhsh morede nazar va fara khani un 
-        optionIcon.forEach(function( v , i){
-            if(i == index){
-                optionIcon[i].classList.toggle('hidden');
-                ـselected+=i
-            }else{
-                optionIcon[i].classList.add("hidden");
-            }
-        }) 
-    })
+addBtn.addEventListener('click' , function(){
+    _countT =Number(counter.innerHTML)
+    if(_countT>0){
+        console.log(options)
+        options.forEach((value , index)=>{
+            options[index].addEventListener('click' , function(){
+                ـselected=0
+                //peyda kardan bakhsh morede nazar va fara khani un 
+                optionIcon.forEach(function( v , i){
+                    if(i == index){
+                        optionIcon[i].classList.toggle('hidden');
+                        ـselected+=i
+                    }else{
+                        optionIcon[i].classList.add("hidden");
+                    }
+                }) 
+            })
+        })
+    }
 })
 
 
 
+
+
+
+
+
+
+
+//options.forEach((value , index)=>{
+    
+    //         options[index].addEventListener('click' , function(){
+    //             ـselected=0
+    //             //peyda kardan bakhsh morede nazar va fara khani un 
+    //             optionIcon.forEach(function( v , i){
+    //                 if(i == index){
+    //                     optionIcon[i].classList.toggle('hidden');
+    //                     ـselected+=i
+    //                 }else{
+    //                     optionIcon[i].classList.add("hidden");
+    //                 }
+    //             }) 
+    //         })
+    //     })
 
 // hide the menu when a click event occurs outside the menu
 // document.addEventListener('click', (event) => {
